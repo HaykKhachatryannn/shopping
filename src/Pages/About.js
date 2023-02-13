6b8a4b02297e23a2a73d3466ede261916bade55d
HaykKhachatryannn <kbe.one.0407@gmail.com>
2023-02-13 18:14:43 +0400

    a

---
+++ src/Pages/About.js
@@ -0,0 +1,18 @@
+import { products } from "../Components/products";
+export default function About() {
+    return (
+        <>
+            <div className="products">
+                <div className="product">
+                    {products.map(product => {
+                        return <div className="product-item" key={product.id}>
+                            <h3>{product.category}</h3>
+                            <img src={product.picture} />
+                            <button>Watch More</button>
+                        </div>
+                    })}
+                </div>
+            </div>
+        </>
+    )
+}
